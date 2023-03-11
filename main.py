1b7c9c3beeef1215c6cc0b68b53c7ed275f1140f
mzlegion <mzhao.ouo@gmail.com>
2023-03-11 19:44:28 +0800

    重新设计了用户界面

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ with gr.Blocks() as demo:
         with gr.Column(scale=1):
             with gr.Box():
                 gr.Markdown("**OpenAI API-Key**")
-                api_key = gr.Textbox(show_label=False, placeholder="Please enter your OpenAI API-key",label="OpenAI API-Key", value=args["api-key"], type="password").style(container=False)
+                api_key = gr.Textbox(show_label=False, placeholder="请在这里输入你的API-key", value=args["api-key"], type="password").style(container=False)
         with gr.Column(scale=3):
             with gr.Box():
-                gr.Markdown("**Select Index**")
+                gr.Markdown("**选择索引**")
                 with gr.Row():
                     with gr.Column(scale=12):
                         index_select = gr.Dropdown(choices=refresh_json_list(plain=True), show_label=False).style(container=False)
@@ -30,53 +30,43 @@ with gr.Blocks() as demo:
                         index_refresh_btn = gr.Button("🔄").style()
         
         
-    with gr.Tab("Chat"):
+    with gr.Tab("对话"):
         with gr.Row():
             with gr.Column(scale=1):
-                chat_tone = gr.Radio(["创意", "平衡", "精确"], label="Chatbot Tone", type="index", value="平衡")
+                chat_tone = gr.Radio(["创意", "平衡", "精确"], label="语气", type="index", value="平衡")
             with gr.Column(scale=3):
-                search_options_checkbox = gr.CheckboxGroup(label="Search Option", choices=["🔍 New Google", "🔍 New Baidu", "🔍 Customize"])
+                search_options_checkbox = gr.CheckboxGroup(label="搜索选项", choices=["🔍 New Google", "🔍 New Baidu", "🔍 手动输入"])
         chatbot = gr.Chatbot()
         with gr.Row():
             with gr.Column(min_width=50, scale=1):
                 chat_empty_btn = gr.Button("🧹", variant="secondary")
             with gr.Column(scale=12):
-                chat_input = gr.Textbox(show_label=False, placeholder="Type here...").style(container=False)
+                chat_input = gr.Textbox(show_label=False, placeholder="在此输入...").style(container=False)
             with gr.Column(min_width=50, scale=1):
                 chat_submit_btn = gr.Button("🚀", variant="primary")
-        suggested_user_turns = gr.Dropdown(choices=[], label="Suggested User Turns")
+        suggested_user_turns = gr.Dropdown(choices=[], label="推荐的回复")
 
 
-    with gr.Tab("Ask"):
-        with gr.Box():
-            with gr.Column():
-                gr.Markdown("## Ask")
-                with gr.Column():
-                    with gr.Accordion("Prompt Template", open=False):
-                        with gr.Row():
-                            sim_k = gr.Slider(1, 10, 1, step=1, label="The Number of Similarity chunks", interactive=True, show_label=True)
-                            tempurature = gr.Slider(0, 2, 0.5, step=0.1, label="Temperature", interactive=True, show_label=True)
-                        tmpl_select = gr.Radio(prompt_tmpl_list, value="Default", label="pre-prompt-template", interactive=True)
-                        prompt_tmpl = gr.Textbox(value=prompt_tmpl_dict["Default"], show_label=False)
-                    query_box = gr.Textbox(lines=3, show_label=False).style(container=False)
-                query_btn = gr.Button("🚀", variant="primary")
-        with gr.Box():
-            gr.Markdown("## Result")
-            answer = gr.Markdown("")
+    with gr.Tab("对话设置"):
+        with gr.Row():
+            sim_k = gr.Slider(1, 10, 1, step=1, label="尝试次数", interactive=True, show_label=True)
+            tempurature = gr.Slider(0, 2, 0.5, step=0.1, label="回答灵活性", interactive=True, show_label=True)
+        tmpl_select = gr.Radio(prompt_tmpl_list, value="MZhao Mode", label="Prompt模板", interactive=True)
+        prompt_tmpl = gr.Textbox(value=prompt_tmpl_dict["MZhao Mode"] ,lines=10, max_lines=40 ,show_label=False)
 
 
-    with gr.Tab("自定index"):
+    with gr.Tab("构建索引"):
         with gr.Row():
             with gr.Column():
-                upload_file = gr.Files(label="Upload Files(Support .txt, .pdf, .epub, .docx)")
+                upload_file = gr.Files(label="上传文件(支持 .txt, .pdf, .epub, .docx等)")
                 with gr.Row():
-                    max_input_size = gr.Slider(256, 4096, 4096, step=1, label="Max Input Size", interactive=True, show_label=True)
-                    num_outputs = gr.Slider(256, 4096, 512, step=1, label="Num Outputs", interactive=True, show_label=True)
+                    max_input_size = gr.Slider(256, 4096, 4096, step=1, label="提问tokens限制", interactive=True, show_label=True)
+                    num_outputs = gr.Slider(256, 4096, 512, step=1, label="回答tokens限制", interactive=True, show_label=True)
                 with gr.Row():
-                    max_chunk_overlap = gr.Slider(0, 100, 20, step=1, label="Max Chunk Overlap", interactive=True, show_label=True)
-                    chunk_size_limit = gr.Slider(256, 4096, 512, step=1, label="Chunk Size Limit", interactive=True, show_label=True)
-                new_index_name = gr.Textbox(placeholder="New Index Name", show_label=False).style(container=False)
-                construct_btn = gr.Button("Construct", variant="primary")
+                    max_chunk_overlap = gr.Slider(0, 100, 20, step=1, label="选段重复度（单位tokens）", interactive=True, show_label=True)
+                    chunk_size_limit = gr.Slider(256, 4096, 512, step=1, label="选段长度限制", interactive=True, show_label=True)
+                new_index_name = gr.Textbox(placeholder="新索引名称：", show_label=False).style(container=False)
+                construct_btn = gr.Button("⚒️ 构建", variant="primary")
             with gr.Row():
                 with gr.Column():
                     with gr.Row():
@@ -86,17 +76,17 @@ with gr.Blocks() as demo:
                             json_select = gr.Dropdown(choices=refresh_json_list(plain=True), show_label=False, multiselect=False).style(container=False)
                         with gr.Column(min_width=50, scale=1):
                             json_confirm_btn = gr.Button("🔎")
-                    json_display = gr.JSON(label="View index json")
-
+                    json_display = gr.JSON(label="查看所选json文件")
+               
     index_refresh_btn.click(refresh_json_list, None, [index_select])
-    query_btn.click(ask_ai, [api_key, index_select, query_box, prompt_tmpl, sim_k, tempurature], [answer])
-    tmpl_select.change(change_prompt_tmpl, [tmpl_select], [prompt_tmpl])
-
+               
     chat_input.submit(chat_ai, [api_key, index_select, chat_input, prompt_tmpl, sim_k, chat_tone, chat_context, chatbot, search_options_checkbox, suggested_user_turns], [chat_context, chatbot, suggested_user_turns])
     chat_input.submit(reset_textbox, [], [chat_input])
     chat_submit_btn.click(chat_ai, [api_key, index_select, chat_input, prompt_tmpl, sim_k, chat_tone, chat_context, chatbot, search_options_checkbox, suggested_user_turns], [chat_context, chatbot, suggested_user_turns])
     chat_submit_btn.click(reset_textbox, [], [chat_input])
     chat_empty_btn.click(lambda: ([], []), None, [chat_context, chatbot])
+    
+    tmpl_select.change(change_prompt_tmpl, [tmpl_select], [prompt_tmpl])
 
     construct_btn.click(construct_index, [api_key, upload_file, new_index_name, max_input_size, num_outputs, max_chunk_overlap], [index_select, json_select])
     json_confirm_btn.click(display_json, [json_select], [json_display])
